876135ad07e3d921653560cecbc3936fd3991465
Ivan S <92943330+MarioInMilo@users.noreply.github.com>
2022-07-09 20:22:17 +0300

    Delete HW0907 directory

--- HW0907/task#2/task1/scr.js
+++
@@ -1,7 +0,0 @@
-$(".item").mouseover(function () {
-	$(this).css('backgroundColor', 'orange');
-});
-
-$(".item").mouseout(function () {
-	$(this).css('backgroundColor', 'transparent');
-});
--- HW0907/task#2/task2/scr.js
+++
@@ -1,5 +0,0 @@
-let count = 0;
-$(".general").click(function () {
-	count = count + 1;
-	$(".p").html(count);
-});
